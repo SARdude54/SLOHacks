b0b0a531101cdde1f0a803f53d928e0b12a1295b
Saul <saulargz35@gmail.com>
2023-05-13 23:24:55 -0700

    fixed news link

--- data.py
+++ data.py
@@ -5,6 +5,7 @@ def get_data(file="static/Mapping_Police_Violence.csv") -> list[dict]:
     with open(file, mode="r", encoding="utf-8") as f:
         reader = csv.reader(f, delimiter=",")
         for row in reader:
+            news_list = row[17].split("\n\n")
             data = {
                 "name": row[0] if len(row[0]) != 0 else "NA",
                 "age": row[1] if len(row[1]) != 0 else "NA",
@@ -16,7 +17,7 @@ def get_data(file="static/Mapping_Police_Violence.csv") -> list[dict]:
                 "county": row[10] if len(row[10]) != 0 else "NA",
                 "cause_of_death": row[13] if len(row[13]) != 0 else "NA",
                 "circumstance": row[14] if len(row[14]) != 0 else "NA",
-                "news_link": row[17] if len(row[17]) != 0 else "NA",
+                "news_link": news_list,
                 "case_status": row[15] if len(row[15]) != 0 else "NA",
                 "officer_charged": row[16] if len(row[16]) != 0 else "NA"
 
