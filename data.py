a2a90276d59f8623651ed1b39f7e1a853d06b823
Saul <saulargz35@gmail.com>
2023-05-13 22:56:36 -0700

    added more data to results page

--- data.py
+++ data.py
@@ -14,12 +14,11 @@ def get_data(file="static/Mapping_Police_Violence.csv") -> list[dict]:
                 "city": row[7] if len(row[7]) != 0 else "NA",
                 "state": row[8] if len(row[8]) != 0 else "NA",
                 "county": row[10] if len(row[10]) != 0 else "NA",
-                "agency_responsible": row[11] if len(row[11]) != 0 else "NA",
                 "cause_of_death": row[13] if len(row[13]) != 0 else "NA",
                 "circumstance": row[14] if len(row[14]) != 0 else "NA",
                 "news_link": row[17] if len(row[17]) != 0 else "NA",
                 "case_status": row[15] if len(row[15]) != 0 else "NA",
-                "officer_charged": row[16] if len(row[15]) != 0 else "NA"
+                "officer_charged": row[16] if len(row[16]) != 0 else "NA"
 
             }
             lst.append(data)
