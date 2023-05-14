4a891ca401cbd66ff40c0200f4b50888d533d79c
Saul <saulargz35@gmail.com>
2023-05-13 18:28:06 -0700

    added csv file and parsed it

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from data import get_data
 
 app = Flask(__name__)
 
+print(get_data()[1])
+
 @app.route("/")
 def home():
-    return "Hello World"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True);
